e3b113dd6b47fc0ff9654c04b87b34d704ec4157
nayan <nayant01@gmail.com>
2026-02-17 18:13:18 +0530

    the backend registrationn vrify and login api uild suscessfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import { verifyMail } from "../emailVerify/verifyMail.js";
 import { User } from "../models/userModel.js";
+import { Session } from "../models/sessionModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -24,5 +25,75 @@ export const registerUser=async(req,res)=>{
         return res.status(201).json({message:'User registered successfully',success:true,user:newUser})
     }catch(error){
         return res.stastus(500).json({message:'Internal Server Error',sucess:false  })
+    }
+}
+
+export const verifyUser=async(req,res)=>{
+    try {
+        const  authHeader=req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
+            return res.status(401).json({message:'Authorization token is missing or invalid',success:false})
+        }
+        const token=authHeader.split(' ')[1];
+        let decoded;
+        try{
+            decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+        }catch(e){
+             if(e.name === 'TokenExpiredError'){
+            return res.status(400).json({message:'Token has expired',success:false})
+        }
+        return res.status(400).json({message:'token verfication failed',success:false})    
+        }
+        const user=await User.findById(decoded.id);
+        if(!user){
+            return res.status(404).json({message:'User not found',success:false})
+        }
+        user.token=null;
+        user.isvarified=true;
+        await user.save();
+        return res.status(200).json({message:'Email verified successfully',success:true})
+    }catch (error) {
+       return res.status(500).json({message:'Internal Server Error',success:false})
+
+    }
+}
+
+
+export const loginUser=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:'All fields are required',success:false})
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'unothorized access', success: false });
+        }
+        const passwrodCheck=await bcrypt.compare(password,user.password);
+        if(!passwrodCheck){
+            return res.status(402).json({message:'Incorrect password',success:false})
+        }
+        //check if user is verified
+        if (!user.isvarified) {
+            return res.status(403).json({message:'Please verify your email before logging in',success:false})
+        }
+        //check for exisiting session and delet it 
+        const existingSession=await Session.findOne({userId:user._id});
+        if(existingSession){
+            await Session.deleteOne({userId:user._id});
+        }
+
+        //create new session
+        await Session.create({userId:user._id});
+
+        //generate JWT token
+        const acessToken=jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'10d'});  
+        const refreshToken=jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'30d'});
+
+        user.isLoggedIn=true;
+        await user.save();
+        return res.status(200).json({message:`welcome back ${user.username}`,success:true,acessToken,refreshToken})
+    }catch(error){
+        return res.status(500).json({message:'Internal Server Error',success:false}) 
     }
 }
--- backend/router/userRoute.js
+++ backend/router/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser, verifyUser } from "../controllers/userController.js";
 
 const router=express.Router();
 
 router.post("/register",registerUser); 
+router.post("/verify",verifyUser); 
+router.post("/login",loginUser); 
     
-export default router;
+export default router;  
